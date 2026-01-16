dd1394b93994a33ec877c47e25fe8c0c3c8b4e91
kuramagod <raxutogame@gmail.com>
2026-01-16 18:17:25 +0300

    Добавил отзывы в html через шаблонизатор

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 from fastapi import FastAPI, Request
 from routers import product, review, category
-from database import create_dn_and_tables
+from database import create_dn_and_tables, SessionDep
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+
 
 app.include_router(product.router)
 app.include_router(review.router)
@@ -22,5 +23,10 @@ def on_startup():
     create_dn_and_tables()
 
 @app.get("/")
-def main_page(request: Request):
-    return template_obj.TemplateResponse("index.html", {"request": request})
+def main_page(
+    request: Request,
+    session: SessionDep
+    ):
+    reviews = review.read_reviews(session)
+    
+    return template_obj.TemplateResponse("index.html", {"request": request, "reviews": reviews})
